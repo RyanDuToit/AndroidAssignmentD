4da8be0ecd1810dcbdcb5c366ecf29c012078ed2
Ryan DuToit <Ryan.DuToit@drake.edu>
2013-02-17 11:50:58 -0600

    added changes for my screen

--- src/edu/drake/androidactivities/MainActivity.java
+++ src/edu/drake/androidactivities/MainActivity.java
@@ -1,15 +1,40 @@
 package edu.drake.androidactivities;
 
 import android.os.Bundle;
+import android.widget.*;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
-
+	Button buttonAlex;
+	Button buttonPhillip;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		buttonAlex = (Button) findViewById(R.id.button1);
+		buttonPhillip = (Button) findViewById(R.id.button2);
+		
+		buttonAlex.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), AlexActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		
+		buttonPhillip.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), PhillipActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
@@ -18,5 +43,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
 }
