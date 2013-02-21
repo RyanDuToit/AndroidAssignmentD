9c2c0d498dbee8aff5eb091c73ad7b8209f85835
Alexander-Nitschke <alexander.nitschke@drake.edu>
2013-02-20 20:15:36 -0600

    Update src/edu/drake/androidactivities/AlexActivity.java

--- src/edu/drake/androidactivities/AlexActivity.java
+++ src/edu/drake/androidactivities/AlexActivity.java
@@ -44,25 +44,3 @@ public class AlexActivity extends Activity {
 	}
 
 }
-package edu.drake.androidactivities;
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-
-public class AlexActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_alex);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_alex, menu);
-		return true;
-	}
-
-}
